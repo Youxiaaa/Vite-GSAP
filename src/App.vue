04ddfa78aedbe48152a0f3ae0387f7c3f773f9d4
Ryan <56096817+Youxiaaa@users.noreply.github.com>
2022-10-20 00:18:36 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -25,33 +25,34 @@
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: '.boxContainer',
-        start: 'top top',
-        end: '+=1000px',
-        pin: true,
+        start: 'top 10%',
+        // end: '+=1000px',
+        // pin: true,
         scrub: true
       }
     })
     tl.to('.box', {
-      top: '0%',
-      left: '100%',
-      xPercent: '-100',
-      duration: 10,
-      position: 'absolute'
-    }).to('.box', {
       top: '50%',
-      duration: 20,
+      left: '51%',
+      xPercent: '-100',
+      duration: 1,
       position: 'absolute'
-    }).to('.box', {
+    }).to('.box2', {
+      top: '100%',
       left: '50%',
+      duration: 1,
+      rotate: '90',
       xPercent: '-50',
-      duration: 30,
       position: 'absolute'
     }).to('.box', {
       top: '90%',
-      yPercent: '-100',
-      duration: 40,
+      // left: '51%',
+      // xPercent: '-100',
+      left: '52%',
+      rotate: '136',
+      duration: 1,
       position: 'absolute'
-    })
+    }, '<')
 
     const tl2 = gsap.timeline({
       scrollTrigger: {
@@ -65,7 +66,7 @@
     tl2.to('.img1', {
       xPercent: '-100'
     }).to('.img2', {
-      xPercent: '-100'
+      xPercent: '100'
     })
 
     const tl3 = gsap.timeline({
@@ -80,6 +81,21 @@
     tl3.to('.scaleImg', {
       width: '500px'
     })
+
+    const tl4 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.scaleContainer2',
+        start: 'top 35%',
+        end: 'bottom 90%',
+        markers: true,
+        toggleActions: 'play reset reset none'
+      }
+    })
+    tl4.to('.scaleImg2', {
+      duration: 1.5,
+      ease: 'expo',
+      scale: '1'
+    })
     gsapAnimation()
   })
 </script>
@@ -89,8 +105,11 @@
     <div class="min-h-300vh bg-black overflow-x-hidden">
       <h2 class="showText whitespace-nowrap text-100px text-stroke-1px text-stroke-white text-transparent font-bold inline-block">Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan</h2>
       <div class="relative w-full min-h-[calc(100vh-100px)] boxContainer">
-        <div class="box w-100px h-100px border-4px border-black">
-          <img src="https://cdn-icons-png.flaticon.com/512/706/706195.png" alt="" class="w-full h-full object-cover">
+        <div class="box absolute w-100px h-100px top-50% -translate-y-50% rotate-46">
+          <img src="https://cdn-icons-png.flaticon.com/512/4334/4334098.png" alt="" class="w-100px object-cover">
+        </div>
+        <div class="box2 absolute w-100px h-100px top-50% left-50% -translate-y-50%">
+          <img src="https://cdn-icons-png.flaticon.com/512/415/415682.png" alt="" class="w-100px object-cover">
         </div>
       </div>
       <div class="relative">
@@ -104,7 +123,9 @@
       <div class="bg-white w-full min-h-screen flex items-center justify-center scaleContainer">
         <img src="https://images.unsplash.com/photo-1601823984263-b87b59798b70?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" class="scaleImg w-200px object-contain">
       </div>
-      <div class="bg-black w-full min-h-screen"></div>
+      <div class="bg-black w-full min-h-screen flex items-center justify-center scaleContainer2">
+        <img src="https://images.unsplash.com/photo-1605523746900-8aa30db1befd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" class="scaleImg2 w-500px object-contain scale-0">
+      </div>
     </div>
   </div>
 </template>
