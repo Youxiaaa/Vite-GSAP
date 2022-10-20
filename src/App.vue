01bb617803238b1bb9a4e1a448426ddc2a294737
Ryan <twquannon@gmail.com>
2022-10-20 10:57:47 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,10 @@
       scrollTrigger: {
         trigger: '.boxContainer',
         start: 'top top',
-        end: '+=1000px',
-        pin: true,
+        // 增加滾動長度
+        // end: '+=1000px',
+        end: 'bottom top',
+        // pin: true,
         scrub: true
       }
     })
@@ -90,21 +92,20 @@
       <h2 class="showText whitespace-nowrap text-100px text-stroke-1px text-stroke-white text-transparent font-bold inline-block">Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan</h2>
       <div class="relative w-full min-h-[calc(100vh-100px)] boxContainer">
         <div class="box w-100px h-100px border-4px border-black">
-          <img src="https://cdn-icons-png.flaticon.com/512/706/706195.png" alt="" class="w-full h-full object-cover">
+          <img src="https://img.icons8.com/doodle/344/jake.png" alt="" class="w-full h-full object-cover">
         </div>
       </div>
       <div class="relative">
         <h2 class="showText2 whitespace-nowrap -right-0 bottom-0 flex-none text-100px text-stroke-1px text-stroke-white text-transparent font-bold inline-block">Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan Japan</h2>
       </div>
       <div class="relative w-full min-h-screen imgContainer overflow-hidden">
-        <img src="https://images.unsplash.com/photo-1545569341-9eb8b30979d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" class="img1 w-full h-full absolute top-0 left-0 object-cover z-20">
-        <img src="https://images.unsplash.com/photo-1490806843957-31f4c9a91c65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" class="img2 w-full h-full absolute top-0 left-0 object-cover z-10">
-        <img src="https://images.unsplash.com/photo-1542051841857-5f90071e7989?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" class="img3 w-full h-full absolute top-0 left-0 object-cover z-0">
+        <img src="https://images.unsplash.com/photo-1596332996407-d8d5d3b60e8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" class="img1 w-full h-full absolute top-0 left-0 object-cover object-bottom z-20">
+        <img src="https://images.unsplash.com/photo-1526298386254-516d688c9e2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1726&q=80" class="img2 w-full h-full absolute top-0 left-0 object-cover object-bottom z-10">
+        <img src="https://images.unsplash.com/photo-1509023464722-18d996393ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" class="img3 w-full h-full absolute top-0 left-0 object-cover z-0">
       </div>
       <div class="bg-white w-full min-h-screen flex items-center justify-center scaleContainer">
         <img src="https://images.unsplash.com/photo-1601823984263-b87b59798b70?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" class="scaleImg w-200px object-contain">
       </div>
-      <div class="bg-black w-full min-h-screen"></div>
     </div>
   </div>
 </template>
